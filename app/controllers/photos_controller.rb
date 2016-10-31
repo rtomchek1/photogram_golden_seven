cc84aedd10ca352eca6782caabb17014fe9d6e2b
rtomchek <rtomchek@chicagobooth.edu>
2016-10-31 14:46:58 +0000

    FOR PROF BETINA

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,39 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({:id => params[:id]})
+    render("/photos/details.html.erb")
+  end
 
+  def new_form
+    render("/photos/new_form.html.erb")
+  end
+
+  def create_row
+    p = Photo.new
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+    redirect_to("/photos")
+  end
+
+  def destroy
+    Photo.find_by({:id => params[:id]}).destroy
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({:id => params[:id]})
+    render("/photos/edit_form.html.erb")
+  end
+
+  def update
+    p = Photo.find_by({:id => params[:id]})
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    @photo = p
+    render("/photos/details.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,18 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  # Routes to CREATE photos
+  get("/photos/new",       { :controller => "photos", :action => "new_form"})
+  get("/create_photo",     { :controller => "photos", :action => "create_row"})
+
+  # Routes to UPDATE photos
+  get("/photos/:id/edit",  { :controller => "photos", :action => "edit_form"})
+  get("/update_photo/:id",     { :controller => "photos", :action => "update"})
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
+  # Routes to DELETE photos
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy"})
 end
